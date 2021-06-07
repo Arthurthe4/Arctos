41b3c76445dec3801de49c1f03944d278e7a8244
Jonas Wilhelmsen <jonaswilhelmsen97@gmail.com>
2021-06-07 20:09:00 +0200

    Add project files.

---
+++ Arctos/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Arctos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
