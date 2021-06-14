6e398e7fec49ac699bb47c3431948f7b89e747ff
Jonas Wilhelmsen <jonaswilhelmsen97@gmail.com>
2021-06-14 15:45:18 +0200

    delete, edit, create

--- Arctos/Controllers/ApplicationTypeController.cs
+++ Arctos/Controllers/ApplicationTypeController.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace Arctos.Controllers
 {
+
     public class ApplicationTypeController : Controller
     {
         private readonly ApplicationDbContext _db;
@@ -16,7 +17,7 @@ namespace Arctos.Controllers
         {
             _db = db;
         }
-        
+
 
         public IActionResult Index()
         {
@@ -24,6 +25,7 @@ namespace Arctos.Controllers
             return View(objList);
         }
 
+        // --------- CREATE -----------
         // Enter Text, Enter Name, Display Order 
         // GET - CREATE
         public IActionResult Create()
@@ -36,7 +38,68 @@ namespace Arctos.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(ApplicationType obj)
         {
-            _db.ApplicationType.Add(obj);
+            if (ModelState.IsValid)
+            {
+                _db.ApplicationType.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+        // --------- EDIT -----------
+        // GET - EDIT
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0) { return NotFound(); }
+
+            var obj = _db.ApplicationType.Find(id);
+
+            if (obj == null) { return NotFound(); }
+
+            return View(obj);
+        }
+
+        // POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ApplicationType obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.ApplicationType.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+        // --------- DELETE -----------
+        // GET - Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0) { return NotFound(); }
+
+            var obj = _db.ApplicationType.Find(id);
+
+            if (obj == null) { return NotFound(); }
+
+            return View(obj);
+        }
+
+        // POST - DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.ApplicationType.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.ApplicationType.Remove(obj);
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
--- Arctos/Models/ApplicationType.cs
+++ Arctos/Models/ApplicationType.cs
@@ -11,6 +11,7 @@ namespace Arctos.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
 
 
